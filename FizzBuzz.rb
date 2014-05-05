6378933f666046b0831246f51b7086f101dc0400
rsedighi <rsedighi@gmail.com>
2014-05-05 15:15:53 -0700

    Update FizzBuzz.rb

--- FizzBuzz.rb
+++ FizzBuzz.rb
@@ -4,14 +4,14 @@ number= gets.chomp.to_i
 
 if number == 0
   puts 0
-elsif number %3 == 0
-  puts 'Fizz'
-elsif number %5 == 0
-  puts 'Buzz'
 elsif  number %15 == 0
   puts 'FizzBuzz'
-
+elsif number %5 == 0
+  puts 'Buzz'
+elsif number %3 == 0
+  puts 'Fizz'
 else puts number
+
 end
 
 
